9504fd77a850f383de00c412b443552a25ad7347
Abdulrahman9907 <10939@holbertonstudents.com>
2025-07-19 20:49:47 +0300

    g

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 # filepath: /Users/abdulrahmanalhussainy/Documents/Website/2-print.py
+def islower(c):
+    return ord(c) >= 97 and ord(c) <= 122
+
 for i in range(10):
     for j in range(i + 1, 10):
         print("{:d}{:d}".format(i, j), end=", " if not (i == 8 and j == 9)
