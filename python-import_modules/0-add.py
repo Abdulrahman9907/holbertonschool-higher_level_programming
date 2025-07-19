0f2fa78176eb9a65ed483575c934087987ff3dbb
Abdulrahman9907 <10939@holbertonstudents.com>
2025-07-19 22:26:57 +0300

    g

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# filepath: /Users/abdulrahmanalhussainy/Documents/Website/2-print.py
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
