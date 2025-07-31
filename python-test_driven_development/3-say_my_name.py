8e56784e89a9ab738064eb1b0ab32ddc5df0f750
Abdulrahman9907 <10939@holbertonstudents.com>
2025-07-31 16:31:19 +0300

    g

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,11 +8,11 @@ with appropriate type checking for string inputs.
 
 def say_my_name(first_name, last_name=""):
     """Print a formatted name string.
-    
+
     Args:
         first_name (str): The first name to print
         last_name (str): The last name to print (optional)
-        
+
     Raises:
         TypeError: If first_name or last_name is not a string
     """
@@ -20,5 +20,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
