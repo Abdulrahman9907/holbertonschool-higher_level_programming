4bf255e7b22f9eedddc49e80a0e150feb14b54a2
Abdulrahman9907 <10939@holbertonstudents.com>
2025-08-13 12:53:06 +0300

    g

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module defining a Student class."""
+
+
+class Student:
+    """A class that defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+
+        filtered_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_dict[attr] = getattr(self, attr)
+        return filtered_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
