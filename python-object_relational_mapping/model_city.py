7f2c5a3358e56866da39ad506ba4bde92d1efcaf
Abdulrahman9907 <10939@holbertonstudents.com>
2025-09-06 18:01:31 +0300

    python

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Definition of the City class"""
+"""
+Contains the class definition of a City
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
+
+
 class City(Base):
-    """City class that inherits from Base Links to the MySQL table cities"""
+    """Representation of a city"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Definition of the State class and an instance Base = declarative_base()"""
+"""
+Contains the class definition of a State and an instance Base = declarative_base()
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
+
+
 class State(Base):
-    """State class that inherits from Base Links to the MySQL table states"""
+    """Representation of a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
